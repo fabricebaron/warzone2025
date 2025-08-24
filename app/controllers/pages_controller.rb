d416d1df3378701550986bb9b73cae6d7e0288ba
FB <fabricebaron@hotmail.com>
2025-08-24 06:29:12 +0200

    2

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,226 @@
+class PagesController < ApplicationController
+	def home
+	
+	end
+	
+	def best1440
+	
+	end
+
+    def best1080
+
+    end
+
+    def best4K
+
+    end
+
+    def bestGPU
+
+    end
+
+    def bestCPU
+
+    end
+    
+    def bestRAM
+
+    end
+
+    def mbamdchoise
+        
+    end
+
+    def mbIntel
+
+    end
+
+    def bestRAM
+
+    end
+
+    def screen
+
+    end 
+
+    def bestNVME
+
+    end
+
+    def bestWATT
+
+    end
+	
+    def bestbox
+
+    end
+
+    def bestAIO
+
+    end
+
+    def mouse
+
+    end
+
+    def keyboard
+
+    end
+
+    def bestGPU4K
+
+    end
+
+    def bestGPU2K
+
+    end
+	
+    def bestGPUHD
+
+    end
+
+    def g3090TI
+
+    end
+
+    def g3090
+
+    end
+
+    def g3080TI
+
+    end
+
+    def g3080
+
+    end
+
+    def g6900XT
+
+    end
+
+    def g6800XT
+
+    end
+
+    def g6800
+
+    end
+
+    def best1440LOW
+
+    end
+
+    def best1440choise
+
+    end
+
+    def best4KLOW
+
+    end
+
+    def best4Kchoise
+
+    end
+
+    def bestRAMchoise
+
+    end
+
+    def bestRAMDDR5
+
+    end
+
+    def g6950XT
+
+    end
+
+    def pc2k1
+
+    end
+
+    def pc4k1
+
+    end
+
+    def screen4K
+
+    end
+
+    def screen2K
+    
+    end
+
+    def screen1080
+
+    end
+
+    def  x570
+
+    end
+
+    def mb58003d
+
+    end
+
+    def b550
+
+    end
+
+    def z690ddr5
+
+    end
+
+    def z690ddr4
+
+    end
+
+    def b660ddr5
+
+    end
+
+    def b660ddr4
+
+    end
+
+    def nvme4to
+
+    end
+
+    def nvme2to
+
+    end
+
+    def nvme1to
+
+    end
+
+    def customamd
+
+    end
+
+    def customintel
+
+    end
+
+    def aioamd
+
+    end
+
+    def aiointel
+    
+    end
+
+    def best1200
+
+    end
+
+    def best1000
+
+    end
+
+    def best850
+
+    end
+
+end
