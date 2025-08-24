0605599bea263a0aa8f1322aff6392ec8116bd50
FB <fabricebaron@hotmail.com>
2025-08-24 07:24:13 +0200

    22

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "warzone-production.up.railway.app"
+  config.hosts << "codwarzone-dee0face63ad.herokuapp.com"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
